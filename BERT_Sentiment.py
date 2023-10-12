b1daed4231db97393bd447005ebe5771b2974f41
Haris Mahmood <haris.mahmood@uconn.edu>
2023-10-11 20:36:04 -0400

    Update BERT_Sentiment.py

--- BERT_Sentiment.py
+++ BERT_Sentiment.py
@@ -14,24 +14,19 @@ data = pd.read_csv("test1.csv")
 # Split the data into training and testing sets
 train_data, test_data = train_test_split(data, test_size=0.2, random_state=42)
 
-# Define the BERT tokenizer and model
 tokenizer = BertTokenizer.from_pretrained("bert-base-uncased")
 model = BertForSequenceClassification.from_pretrained("bert-base-uncased")
 
-# Extract text and labels as separate lists
 train_texts = train_data["text"].tolist()
 train_labels = train_data["label"].tolist()
 
-# Define a function to preprocess the text and labels
 def preprocess_text(texts, labels):
     inputs = tokenizer(texts, return_tensors="pt", truncation=True, padding=True)
     labels = torch.tensor(labels)
     return TensorDataset(inputs["input_ids"], inputs["attention_mask"], labels)
 
-# Preprocess your training data
 train_inputs = preprocess_text(train_texts, train_labels)
 
-# Define a function to fine-tune the model
 def fine_tune_bert(model, train_data, epochs=1, batch_size=8, learning_rate=2e-5):
     dataloader = DataLoader(train_data, batch_size=batch_size, shuffle=True)
     optimizer = AdamW(model.parameters(), lr=learning_rate)
@@ -48,16 +43,12 @@ def fine_tune_bert(model, train_data, epochs=1, batch_size=8, learning_rate=2e-5
             total_loss += loss.item()
         print(f"Epoch {epoch+1} Loss: {total_loss / len(dataloader)}")
 
-# Fine-tune the BERT model
 fine_tune_bert(model, train_inputs)
 
-# Save the fine-tuned model
 model.save_pretrained("fine_tuned_bert_model")
 
-# Load the model for evaluation
 model = BertForSequenceClassification.from_pretrained("fine_tuned_bert_model")
 
-# Define a function for sentiment analysis
 def analyze_sentiment(text):
     inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True)
     outputs = model(**inputs)
